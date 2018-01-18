79b1eaacd00a64dcd0c3a25ebf54f6dc55eeb3f5
柊 佐藤 <150036@jc-21.jp>
2018-01-18 10:33:35 +0900

    変更1

--- MuscleTrainingRecords00/MuscleTrainingRecords00/Training.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/Training.cs
@@ -24,12 +24,12 @@ namespace MuscleTrainingRecords00
         {
             if (Menu.Equals("アブドミナルマシンクランチ（マシン）"))
             {
-                return Menu + "                                                　　　　　     " + Load;
+                return Menu + "                                                　　　　　　　　　　     " + Load;
             }
 
             if (Menu.Length < 27)
             {
-                return String.Format("%27s", Menu )+ "                                                " + Load;
+                return String.Format("%27s", Menu )+ "                                              " + Load;
 
             }
             else
